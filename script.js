5994703d54e04305019afd0cdaa365db2049fb86
ryrmdn <aryrmdn26@gmail.com>
2022-04-09 00:20:23 +0700

    Menambahkan Popup Box

---
+++ script.js
@@ -0,0 +1,20 @@
+alert('Assalamuallaikum..');
+var lagi = true;
+
+while ( lagi === true ) {
+    var nama = prompt('Namanya siapa?');
+    alert('Hallo ' + nama);
+
+    lagi = confirm('Coba lagi?');
+}
+
+alert('Terimakasih');
+alert('Ohiyah, satu lagi nih..')
+var lagi = true;
+
+while ( lagi === true ) {
+    var angka = prompt('nomor WhatsApp nya berapa?');
+    alert('Udah bener Wa nya ' + angka);
+
+    lagi = alert('Oke makasih ya, nanti sleepcall\'an');
+}
